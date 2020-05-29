90b9679bf248e31bbccb6212a3bac5b88cfabf12
Nandha <Nandha@Nandha-PC>
2020-05-29 20:31:39 +0530

    first commit

---
+++ Jenkins/src/com/org/test/Jtest.java
@@ -0,0 +1,24 @@
+package com.org.test;
+
+public class Jtest {
+
+	public static void main(String[] args) {
+		char ch='o'; 
+		switch(ch){
+		case 'a':System.out.println("vowel");
+		break;
+		case 'e':System.out.println("vowel");
+		break;
+		case 'i':System.out.println("vowel");
+		break;
+		case 'o':System.out.println("vowel");
+		break;
+		case 'u':System.out.println("vowel");
+		break;
+		default: System.out.println("not in vowel");
+		}
+	}
+
+}
+
+
